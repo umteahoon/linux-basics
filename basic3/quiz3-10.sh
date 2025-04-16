47e386be9394d852a2ce5c72dcd671a641769a84
umteahoon <freeam2006@gmail.com>
2025-04-16 12:15:35 +0900

    파일 유형 카운트

--- basic3/quiz3-10.sh
+++ basic3/quiz3-10.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-for item in /home/ubuntu/Documents/linux-basics/basic3
+file_count=0
+dir_count=0
+for item in *
 do
-		echo "$item"
+	if [ -d "$item" ]
+	then 
+			dir_count=$((dir_count+1))
+	elif [ -f "$item" ]
+	then 
+			file_count=$((file_count+1))
+	else
+		echo "$item x"
+	fi
 done
+echo "파일 수 : $file_count"
+echo "디렉토리 수 : $dir_count"
+exit 0
